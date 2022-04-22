e9ff9b354ce863a99930bb725e098fd49e889a56
Chris Povirk <cpovirk@google.com>
2022-04-22 11:26:09 -0400

    Try to use @NullMarked on a module.

--- caffeine/src/main/java/module-info.java
+++ caffeine/src/main/java/module-info.java
@@ -1,7 +1,9 @@
+@org.jspecify.nullness.NullMarked
 module com.github.benmanes.caffeine {
   exports com.github.benmanes.caffeine.cache;
   exports com.github.benmanes.caffeine.cache.stats;
 
   requires static com.google.errorprone.annotations;
   requires static org.checkerframework.checker.qual;
+  requires static org.jspecify;
 }
