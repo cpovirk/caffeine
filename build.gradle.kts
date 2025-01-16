d055de1cd48e5422c39d81134ee999dfb0c8fee1
Chris Povirk <cpovirk@google.com>
2025-01-16 16:30:22 -0500

    demo of what should be a successful map call and two failures -- and which is when i run kotlinc directly, but all are errors when i run `gradle tasks`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
 import org.gradle.api.file.DirectoryProperty
+
+import org.gradle.api.provider.Provider
+
 val n: String? = null
 fun go(g: DirectoryProperty) = g.map { n }
+
+fun go2(g: DirectoryProperty): Provider<String> = g.map { n }
+fun go3(g: DirectoryProperty): Provider<String> = g.map(null)
